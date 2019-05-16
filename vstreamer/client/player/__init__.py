bc5d6b859019c1f67de2d5b2e5f0496bdfc5d136
Artur Pietrzyk <artudi54@gmail.com>
2019-05-16 15:45:06 +0200

    Fixed imports

--- vstreamer/client/player/__init__.py
+++ vstreamer/client/player/__init__.py
@@ -1,2 +1,2 @@
-from vstreamer.client.player.VideoPlayer import VideoPlayer
 from vstreamer.client.player.VideoPlayerBar import VideoPlayerBar
+from vstreamer.client.player.VideoPlayer import VideoPlayer
