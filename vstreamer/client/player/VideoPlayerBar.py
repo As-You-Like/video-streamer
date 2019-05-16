20c36fd29dcfa4e046e46a1be13c7af73ca516ad
tkolbusz <tomaszkolbusz1@gmail.com>
2019-05-16 13:37:05 +0200

    fix VideoPlayerBar

--- vstreamer/client/player/VideoPlayerBar.py
+++ vstreamer/client/player/VideoPlayerBar.py
@@ -11,5 +11,5 @@ class VideoPlayerBar(QtWidgets.QWidget):
         self.set_current_video_time(0, 0)
 
     def set_current_video_time(self, current_time_ms, total_time_ms):
-        self.length_label(format_time(current_time_ms) + "/" + format_time(total_time_ms))
+        self.length_label.setText(format_time(current_time_ms) + "/" + format_time(total_time_ms))
         # todo slider
